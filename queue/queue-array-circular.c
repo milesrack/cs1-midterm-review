209c438c77d34f7b74a15aa99e54e97434230a28
user <user@example.com>
2024-02-25 21:30:44 -0500

    Add queue implementations

--- queue/queue-array-circular.c
+++ queue/queue-array-circular.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #define MAX 10
 
+int queue[MAX];
+int front = -1;
+int size = 0;
+
 void enqueue(int val);
 int dequeue();
 int empty();
 int full();
 void print();
-
-int queue[MAX];
-int front = -1;
-int rear = -1;
 
 int main(){
     for (int i = 0; i < MAX; i++){
@@ -21,6 +21,11 @@ int main(){
         dequeue();
     }
     dequeue();
+    for (int i = 0; i < MAX; i++){
+        enqueue(i);
+        print();
+    }
+    enqueue(MAX);
     return 0;
 }
 
@@ -29,29 +34,39 @@ void enqueue(int val){
         printf("Queue is full!\n");
         return;
     }
+    
     if (empty()){
         front = 0;
     }
-    queue[++rear] = val;
+    
+    queue[(front + size) % MAX] = val;
+    size++;
 }
 
 int dequeue(){
-    if (empty() || front > rear){
+    int val;
+
+    if (empty()){
         printf("Queue is empty!\n");
         return -1;
     }
-    return queue[front++];
+
+    val = queue[front];
+    front = (front + 1) % MAX;
+    size--;
+    
+    return val;
 }
 
 int empty(){
-    if (front == -1){
+    if (size == 0){
         return 1;
     }
     return 0;
 }
 
 int full(){
-    if (rear == MAX - 1){
+    if (size == MAX){
         return 1;
     }
     return 0;
@@ -62,8 +77,8 @@ void print(){
         printf("Queue is empty!\n");
         return;
     }
-    for (int i = front; i <= rear; i++){
-        printf("%d ", queue[i]);
+    for (int i = 0; i < size; i++){
+        printf("%d ", queue[(front + i) % MAX]);
     }
     printf("\n");
 }
--- queue/queue-linked-list.c
+++ queue/queue-linked-list.c
@@ -1,6 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void enqueue(int val);
+int dequeue();
+int empty();
+void print();
+
+typedef struct Node{
+    int val;
+    struct Node *next;
+} Node;
+
+Node *front = NULL;
+Node *rear = NULL;
+
 int main(){
+    for (int i = 0; i < 10; i++){
+        enqueue(i);
+        print();
+    }
+    for (int i = 0; i < 10; i++){
+        dequeue();
+        print();
+    }
     return 0;
+}
+
+void enqueue(int val){
+    Node *new = NULL;
+
+    if (empty()){
+        front = (Node *) malloc(sizeof(Node));
+        front->val = val;
+        front->next = NULL;
+        rear = front;
+        return;
+    }
+    rear->next = (Node *) malloc(sizeof(Node));
+    new = rear->next;
+    new->val = val;
+    new->next = NULL;
+    rear = new;
+}
+
+int dequeue(){
+    int val;
+
+    if (empty()){
+        printf("Queue is empty!\n");
+        return -1;
+    }
+
+    val = front->val;
+    front = front->next;
+
+    return val;
+}
+
+int empty(){
+    if (front == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+void print(){
+    Node *cur = front;
+
+    if (empty()){
+        printf("Queue is empty!\n");
+        return;
+    }
+
+    while (cur != NULL){
+        printf("%d ", cur->val);
+        cur = cur->next;
+    }
+    printf("\n");
 }
